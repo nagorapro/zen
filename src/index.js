09bbbd21d6c8fb8d9d43f67913930439ce5281b9
Vitaliy Nagora <vitaliynagora@icloud.com>
2022-06-29 17:02:15 +0300

    refactor(index.js)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import * as ReactDOM from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import {AppProvider} from './context/AppContext'
 import App from './App'
 import './scss/index.scss'
 
 const $root = document.querySelector('#root')
-const root = ReactDOM.createRoot($root)
+const root = createRoot($root)
 
 root.render(
   <AppProvider>
